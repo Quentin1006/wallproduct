7234170121ced8aa7ed07b5454cfdf1275c9ca67
quentin.sahal <quentin.sahal@gmail.com>
2022-10-16 11:01:05 +0200

    first try implementing useDebounce

--- src/modules/WallProduct/components/Filters/TextInputFilter.tsx
+++ src/modules/WallProduct/components/Filters/TextInputFilter.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, Ref, useState } from "react"
+import { Fragment, useRef, useState } from "react"
 import { Filter } from "typings"
 
 export type TextInputFilterProps = {
@@ -7,23 +7,26 @@ export type TextInputFilterProps = {
   onUpdateFilter: (label: string, value: string) => void
 }
 
-const TextInputFilter = React.forwardRef(
-  ({ filterState, name, onUpdateFilter }: TextInputFilterProps, ref: Ref<any>) => {
-    return (
-      <Fragment key={name}>
-        <label htmlFor={name}>{name} : </label>
-        <input
-          name={name}
-          type="text"
-          value={filterState.state}
-          ref={ref}
-          onChange={(e) => {
-            onUpdateFilter(name, e.target.value)
-          }}
-        />
-      </Fragment>
-    )
+const TextInputFilter = ({ filterState, name, onUpdateFilter }: TextInputFilterProps) => {
+  const [tmpValue, setTmpValue] = useState("")
+  let timeoutId = useRef<ReturnType<typeof setTimeout>>()
+
+  console.log("rerendering input")
+
+  const onChange = (e: any) => {
+    clearTimeout(timeoutId.current)
+    setTmpValue(e.target.value)
+    timeoutId.current = setTimeout(() => {
+      onUpdateFilter(name, e.target.value)
+    }, 500)
   }
-)
+
+  return (
+    <Fragment key={name}>
+      <label htmlFor={name}>{name} : </label>
+      <input name={name} type="text" value={tmpValue} onChange={onChange} />
+    </Fragment>
+  )
+}
 
 export default TextInputFilter
