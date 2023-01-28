8b7f4a1b7e1e84337f3b5412a390b808e0515461
quentin.sahal <quentin.sahal@gmail.com>
2023-01-28 11:59:57 +0100

    use faker to generate users

--- src/lib/components/ProductsList/ProductsList.tsx
+++ src/lib/components/ProductsList/ProductsList.tsx
@@ -1,6 +1,5 @@
 import React, { Fragment, ReactNode } from "react"
 import InfiniteScroll from "react-infinite-scroll-component"
-import { observer } from "mobx-react-lite"
 
 import { HeaderContainer } from "./HeaderContainer"
 
@@ -17,61 +16,59 @@ export type ProductsListProps<T extends NamedObj> = {
   listStyle?: React.CSSProperties
 }
 
-export const ProductsList = observer(
-  <T extends NamedObj>({
-    getNextProducts,
-    products,
-    renderProduct,
-    Header,
-    totalLength,
-    headerStyle,
-    listStyle,
-  }: ProductsListProps<T>) => {
-    console.log("rendering ProductsList", products.length)
-    console.log(
-      "has More ? ",
-      products.length < totalLength,
-      ", productsLen",
-      products.length,
-      ", totalLength",
-      totalLength
-    )
+export const ProductsList = <T extends NamedObj>({
+  getNextProducts,
+  products,
+  renderProduct,
+  Header,
+  totalLength,
+  headerStyle,
+  listStyle,
+}: ProductsListProps<T>) => {
+  console.log("rendering ProductsList", products.length)
+  console.log(
+    "has More ? ",
+    products.length < totalLength,
+    ", productsLen",
+    products.length,
+    ", totalLength",
+    totalLength
+  )
 
-    if (!products || products.length === 0) {
-      return <div>No product to display</div>
-    }
-    return (
-      <div style={{ paddingBottom: "50px" }}>
-        {Header ? (
-          <HeaderContainer customStyle={headerStyle}>
-            <Header />
-          </HeaderContainer>
-        ) : null}
-
-        <InfiniteScroll
-          dataLength={products.length}
-          hasMore={products.length < totalLength}
-          next={getNextProducts}
-          loader={<h4>Loading...</h4>}
-          endMessage={
-            <p style={{ textAlign: "center" }}>
-              <b>Yay! You have seen it all</b>
-            </p>
-          }
-        >
-          <div
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-              ...listStyle,
-            }}
-          >
-            {products.map((product) => (
-              <Fragment key={product.id}>{renderProduct(product)}</Fragment>
-            ))}
-          </div>
-        </InfiniteScroll>
-      </div>
-    )
+  if (!products || products.length === 0) {
+    return <div>No product to display</div>
   }
-)
+  return (
+    <div style={{ paddingBottom: "50px" }}>
+      {Header ? (
+        <HeaderContainer customStyle={headerStyle}>
+          <Header />
+        </HeaderContainer>
+      ) : null}
+
+      <InfiniteScroll
+        dataLength={products.length}
+        hasMore={products.length < totalLength}
+        next={getNextProducts}
+        loader={<h4>Loading...</h4>}
+        endMessage={
+          <p style={{ textAlign: "center" }}>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            ...listStyle,
+          }}
+        >
+          {products.map((product) => (
+            <Fragment key={product.id}>{renderProduct(product)}</Fragment>
+          ))}
+        </div>
+      </InfiniteScroll>
+    </div>
+  )
+}
